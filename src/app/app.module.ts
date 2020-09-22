20c5d52c10f4b1839f481253332f0ebf08a6d2e1
Otti Bensghir <bensghoe@users.noreply.github.com>
2020-09-22 13:55:11 -0400

    creat main components using the command line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { SharedComponent } from './shared/shared.component';
+import { ComponentsComponent } from './components/components.component';
+import { ComercialPortalComponent } from './comercial-portal/comercial-portal.component';
+import { ManagmentPortalComponent } from './managment-portal/managment-portal.component';
+import { EmployeePortalComponent } from './employee-portal/employee-portal.component';
+import { CustomerPortalComponent } from './customer-portal/customer-portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    SharedComponent,
+    ComponentsComponent,
+    ComercialPortalComponent,
+    ManagmentPortalComponent,
+    EmployeePortalComponent,
+    CustomerPortalComponent
   ],
   imports: [
     BrowserModule,
